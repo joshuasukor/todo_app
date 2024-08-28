33913f5959739f4e977490cfc0ef26d8460c1617
0138200730 <joshuasukor@outlook.com>
2024-08-28 22:45:08 +0800

    2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_screen.dart';
 import 'package:todo_app/screens/edit_screen.dart';
 import 'package:todo_app/screens/home_screen.dart';
 import 'package:todo_app/utils/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     MaterialApp(
       title: 'To-Do',
       theme: appTheme(),
       routes: {
-        '/': (context) => const HomeScreen(),
+        '/': (context) => HomeScreen(),
         '/add': (context) => const AddScreen(),
         '/edit': (context) => const EditScreen(),
       },
--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+import 'package:todo_app/models/todo.dart';
+import 'package:todo_app/repositories/todo_repository.dart';
 import 'package:todo_app/utils/app_color.dart';
 import 'package:todo_app/widgets/custom_button.dart';
 import 'package:todo_app/widgets/custom_date_picker.dart';
 
-class AddScreen extends StatelessWidget {
+class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
+
+  @override
+  State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  final TextEditingController _title = TextEditingController();
+
+  DateTime _startDate = DateTime.now();
+  DateTime _endDate = DateTime.now();
+
+  String _status = 'Incomplete';
+
+  final todoRepo = TodoRepository();
+
+  addNewTodo() {
+    final title = _title.text;
+    final startDate = _startDate;
+    final endDate = _endDate;
+    final status = _status;
+
+    final data = Todo(
+      title: title,
+      startDate: Jiffy.parseFromDateTime(startDate)
+          .format(pattern: 'dd MMM yyyy, h:mm a'),
+      endDate: Jiffy.parseFromDateTime(endDate)
+          .format(pattern: 'dd MMM yyyy, h:mm a'),
+      status: status,
+    );
+
+    todoRepo.addNewTodo(data).then((_) => Navigator.pop(context));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +64,7 @@ class AddScreen extends StatelessWidget {
               height: 10,
             ),
             TextFormField(
+              controller: _title,
               cursorColor: AppColors.black,
               maxLines: 4,
               textInputAction: TextInputAction.done,
@@ -54,8 +90,14 @@ class AddScreen extends StatelessWidget {
               ),
             ),
             CustomDatePicker(
-              value: null,
-              onDateSelected: (selectedDate) {},
+              firstDate: DateTime.now(),
+              lastDate: DateTime(2050, 01, 01),
+              value: _startDate,
+              onDateSelected: (selectedDate) {
+                setState(() {
+                  _startDate = selectedDate;
+                });
+              },
             ),
             const SizedBox(
               height: 20,
@@ -69,15 +111,21 @@ class AddScreen extends StatelessWidget {
               ),
             ),
             CustomDatePicker(
-              value: null,
-              onDateSelected: (selectedDate) {},
+              firstDate: DateTime.now(),
+              lastDate: DateTime(2050, 01, 01),
+              value: _endDate,
+              onDateSelected: (selectedDate) {
+                setState(() {
+                  _endDate = selectedDate;
+                });
+              },
             ),
           ],
         ),
       ),
       bottomNavigationBar: CustomButton(
-        label: 'Create new',
-        onPressed: () {},
+        label: 'Create Now',
+        onPressed: addNewTodo,
       ),
     );
   }
